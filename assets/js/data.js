a80075682457c9c05d77132040bdd4be79923ad9
chomapublicize <choma@publicize.co>
2022-08-29 19:30:31 -0500

    starter

---
+++ assets/js/data.js
@@ -0,0 +1,112 @@
+[
+    {
+      "id": 1,
+      "name": "Leanne Graham",
+      "username": "Bret",
+      "email": "Sincere@april.biz",
+      "phone": "1-770-736-8031 x56442",
+      "website": "hildegard.org",
+      "description": "lorem ipsum dolor sit amet",
+      "avatar": "https://i.pravatar.cc/300?img=3",
+      "skills": ["html", "python", "php"]
+    },
+    {
+      "id": 2,
+      "name": "Ervin Howell",
+      "username": "Antonette",
+      "email": "Shanna@melissa.tv",
+      "phone": "010-692-6593 x09125",
+      "website": "anastasia.net",
+      "description": "lorem ipsum dolor sit amet",
+      "avatar": "https://i.pravatar.cc/300?img=4",
+      "skills": ["mysql", "ui/ux designer"]
+    },
+    {
+      "id": 3,
+      "name": "Clementine Bauch",
+      "username": "Samantha",
+      "email": "Nathan@yesenia.net",
+      "phone": "1-463-123-4447",
+      "website": "ramiro.info",
+      "description": "lorem ipsum dolor sit amet",
+      "avatar": "https://i.pravatar.cc/300?img=5",
+      "skills": ["html", "php"]
+    },
+    {
+      "id": 4,
+      "name": "Patricia Lebsack",
+      "username": "Karianne",
+      "email": "Julianne.OConner@kory.org",
+      "phone": "493-170-9623 x156",
+      "website": "kale.biz",
+      "description": "lorem ipsum dolor sit amet",
+      "avatar": "https://i.pravatar.cc/300?img=6",
+      "skills": ["html", "js", "php"]
+    },
+    {
+      "id": 5,
+      "name": "Chelsey Dietrich",
+      "username": "Kamren",
+      "email": "Lucio_Hettinger@annie.ca",
+      "phone": "(254)954-1289",
+      "website": "demarco.info",
+      "description": "lorem ipsum dolor sit amet",
+      "avatar": "https://i.pravatar.cc/300?img=7",
+      "skills": ["html", "css"]
+    },
+    {
+      "id": 6,
+      "name": "Mrs. Dennis Schulist",
+      "username": "Leopoldo_Corkery",
+      "email": "Karley_Dach@jasper.info",
+      "phone": "1-477-935-8478 x6430",
+      "website": "ola.org",
+      "description": "lorem ipsum dolor sit amet",
+      "avatar": "https://i.pravatar.cc/300?img=8",
+      "skills": ["html", "css", "jquery"]
+    },
+    {
+      "id": 7,
+      "name": "Kurtis Weissnat",
+      "username": "Elwyn.Skiles",
+      "email": "Telly.Hoeger@billy.biz",
+      "phone": "210.067.6132",
+      "website": "elvis.io",
+      "description": "lorem ipsum dolor sit amet",
+      "avatar": "https://i.pravatar.cc/300?img=9",
+      "skills": ["next.js", "react", "bootstrap 4"]
+    },
+    {
+      "id": 8,
+      "name": "Nicholas Runolfsdottir V",
+      "username": "Maxime_Nienow",
+      "email": "Sherwood@rosamond.me",
+      "phone": "586.493.6943 x140",
+      "website": "jacynthe.com",
+      "description": "lorem ipsum dolor sit amet",
+      "avatar": "https://i.pravatar.cc/300?img=10",
+      "skills": ["html", "angular", "react"]
+    },
+    {
+      "id": 9,
+      "name": "Glenna Reichert",
+      "username": "Delphine",
+      "email": "Chaim_McDermott@dana.io",
+      "phone": "(775)976-6794 x41206",
+      "website": "conrad.com",
+      "description": "lorem ipsum dolor sit amet",
+      "avatar": "https://i.pravatar.cc/300?img=11",
+      "skills": ["html", "css", "javscript"]
+    },
+    {
+      "id": 10,
+      "name": "Clementina DuBuque",
+      "username": "Moriah.Stanton",
+      "email": "Rey.Padberg@karina.biz",
+      "phone": "024-648-3804",
+      "website": "ambrose.net",
+      "description": "lorem ipsum dolor sit amet",
+      "avatar": "https://i.pravatar.cc/300?img=12",
+      "skills": ["css", "wordpress", "php"]
+    }
+  ]
---
+++ assets/js/scripts.js
@@ -0,0 +1,78 @@
+const url = "../assets/js/data.js";
+const elTestData = document.querySelector('#data');
+const elRandomCopy = document.querySelector('#randomCopy');
+const search = document.querySelector( "#searchUser" );
+const randomCopy = ["python developer", "ui/ux designer", "php developer", "javascript developer", "js developer"];
+
+showRandomCopy();
+
+function showRandomCopy() {
+    const generateRandomCopy = randomCopy[Math.floor(Math.random() * randomCopy.length)];
+    return elRandomCopy.innerHTML+= generateRandomCopy;
+}
+
+
+
+const templateString = ( el ) => {
+    let templateString = `
+        <div class="col-lg-4 mb-4">
+            <div class="card h-100">
+
+                <div class="card__image">
+                    <img src="${el.avatar}" class="card-img-top" alt="${el.name}">
+                </div>
+
+                <div class="card-body">
+                    <h5 class="card-title">${el.name}</h5>
+                    
+                    <p class="card-text">${el.description}</p>
+
+                    <div class="skills mb-4">
+                        ${el.skills.map(function (key) {
+                            return "<span class='badge badge-primary'>" + key + "</span>"
+                        }).join(" ")}
+                    </div>
+                    <a href="#" class="btn btn-danger">Hire me!</a>
+                </div>
+            </div>
+        </div>
+    `
+    return templateString;
+}
+
+function printUsers(arr){
+    arr.forEach(function(el){
+        let getTemplate = templateString(el)
+        return elTestData.innerHTML+= getTemplate;
+        
+    })
+}
+
+const getData = async () => {
+    const fetching = await fetch(url).then(response => response.json());
+    
+    return printUsers(fetching)
+}
+
+getData();
+
+
+const lookingForUser = async () => {
+    elTestData.innerHTML = ""
+
+    const fetching = await fetch(url).then(response => response.json());
+
+    const result = fetching.map(item => {
+        const findAtSkills = item.skills.find(el => el.includes(search.value));
+        if(findAtSkills) return item;
+    })
+
+    const clearResults = result.filter(element => { return element !== undefined; })
+
+    if( clearResults.length < 1 ) return elTestData.innerHTML+= `<h1>we couldn't what you looking for, try again!</h1>` 
+    
+    return printUsers(clearResults)
+    
+}
+
+
